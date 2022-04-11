15f833f95d2ce62f7a99a9684d3461d9d394f90f
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-04-11 20:23:26 +0530

    Unify database tables into just two

--- sqlScripts/final.sql
+++ sqlScripts/final.sql
@@ -1,5 +1,3 @@
-
-
 CREATE TABLE student_auth (
     prn VARCHAR(12) PRIMARY KEY,
     password TEXT NOT NULL,
@@ -19,32 +17,31 @@ CREATE TABLE student_data (
     fathers_name TEXT NOT NULL,
     mothers_name TEXT NOT NULL,
     date_of_birth DATE NOT NULL,
+    religion VARCHAR(100) NOT NULL,
     region VARCHAR(50) NOT NULL,
     mother_tongue VARCHAR(100) NOT NULL,
     annual_income_start INT NOT NULL,
     annual_income_end INT NOT NULL,
+
+    line_1 text NOT NULL,
+    line_2 text NOT NULL,
+    line_3 text DEFAULT NULL,
+    state VARCHAR(100) NOT NULL,
+    district VARCHAR(100) NOT NULL,
+    taluka VARCHAR(100) NOT NULL,
+    village VARCHAR(100) NOT NULL,
+    pincode int NOT NULL,
+
     mobile_number VARCHAR(10) NOT NULL,
     email_address TEXT NOT NULL,
     physically_handicapped BIT(1) NOT NULL,
     linguistic_minority BIT(1) NOT NULL,
     religious_minority BIT(1) NOT NULL,
-    course_name TEXT NOT NULL,
-    admission_date DATE NOT NULL,
-    FOREIGN KEY(prn) REFERENCES student_auth(prn)
-);
 
-CREATE INDEX idx_student_data_app_id ON student_data(application_id);
-
-CREATE INDEX idx_student_data_prn ON student_data(prn);
-
-
-CREATE TABLE student_previous_records (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    prn VARCHAR(12) NOT NULL, 
-    scc_board TEXT NOT NULL,
+    ssc_board TEXT NOT NULL,
     ssc_passing_year YEAR(4),
     ssc_total_percentage DOUBLE NOT NULL,
-    qalifying_exam TEXT DEFAULT NULL,
+    qualifying_exam TEXT DEFAULT NULL,
     hsc_board TEXT DEFAULT NULL,
     hsc_passing_year YEAR(4),
     physics_percentage DOUBLE DEFAULT NULL,
@@ -56,20 +53,13 @@ CREATE TABLE student_previous_records (
     jee_percentile DOUBLE DEFAULT NULL,
     merit_no BIGINT DEFAULT NULL,
     merit_marks DOUBLE DEFAULT NULL,
-    FOREIGN KEY (prn) REFERENCES student_auth(prn) 
-);
 
+    course_name TEXT NOT NULL,
+    admission_date DATE NOT NULL,
 
-CREATE TABLE address(
-    id int AUTO_INCREMENT PRIMARY KEY,
-    prn VARCHAR(12) NOT NULL,
-    line_1 text NOT NULL,
-    line_2 text NOT NULL,
-    line_3 text DEFAULT NULL,
-    state VARCHAR(100) NOT NULL,
-    district VARCHAR(100) NOT NULL,
-    village VARCHAR(100) NOT NULL,
-    pincode int NOT NULL,
     FOREIGN KEY (prn) REFERENCES student_auth(prn)
 );
 
+CREATE INDEX idx_student_data_app_id ON student_data(application_id);
+
+CREATE INDEX idx_student_data_prn ON student_data(prn);
