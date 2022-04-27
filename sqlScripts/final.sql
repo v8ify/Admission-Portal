9a9361b5b9db86ee4de86d1eeeea3d7178a12721
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-04-27 18:49:53 +0530

    Modify sql script for fee_matrix table

--- sqlScripts/final.sql
+++ sqlScripts/final.sql
@@ -2,7 +2,8 @@ CREATE TABLE student_auth (
     prn VARCHAR(12) PRIMARY KEY,
     password TEXT NOT NULL,
     payment_done BIT(1) DEFAULT 0,
-    antiragging_uploaded BIT(1) DEFAULT 0
+    antiragging_uploaded BIT(1) DEFAULT 0,
+    fee_category_approved BIT(1) DEFAULT 0
 );
 
 CREATE TABLE superuser (
@@ -74,8 +75,61 @@ CREATE TABLE student_data (
     -- the values of this column will be 2021
     admission_calendar_year YEAR(4) DEFAULT NULL,
 
+    -- which fee paying category the student belongs to
+    -- this might be different than the student's caste hence
+    -- we have to make a separate column
+    fee_paying_category VARCHAR(100),
+
     FOREIGN KEY (prn) REFERENCES student_auth(prn)
 );
+
+CREATE TABLE fee_matrix (
+    year YEAR(4) NOT NULL,    
+    category VARCHAR(100) NOT NULL,
+    fee int NOT NULL
+);
+
+INSERT INTO fee_matrix(year, category, fee) 
+VALUES 
+    (2021, "OPEN", 108850),
+    (2021, "OBC", 61506),
+    (2021, "SEBC", 61506),
+    (2021, "EWS", 61506),
+    (2021, "EBC", 61506),
+    (2021, "SC", 1850),
+    (2021, "ST", 1850),
+    (2021, "VJNT", 14161),
+    (2021, "SBC", 14161),
+    (2021, "TFWS", 14161),
+    (2021, "J & K", 26180),
+    (2021, "GOI", 26180),
+    (2021, "JKSSS", 2180),
+    (2020, "OPEN", 121270),
+    (2020, "OBC", 67795),
+    (2020, "SEBC", 67795),
+    (2020, "EWS", 67795),
+    (2020, "EBC", 67795),
+    (2020, "SC", 1270),
+    (2020, "ST", 1270),
+    (2020, "VJNT", 14319),
+    (2020, "SBC", 14319),
+    (2020, "TFWS", 14319),
+    (2020, "J & K", 25100),
+    (2020, "GOI", 25100),
+    (2020, "JKSSS", 1100),
+    (2019, "OPEN", 121270),
+    (2019, "OBC", 67795),
+    (2019, "SEBC", 67795),
+    (2019, "EWS", 67795),
+    (2019, "EBC", 67795),
+    (2019, "SC", 1270),
+    (2019, "ST", 1270),
+    (2019, "VJNT", 14319),
+    (2019, "SBC", 14319),
+    (2019, "TFWS", 14319),
+    (2019, "J & K", 25100),
+    (2019, "GOI", 25100),
+    (2019, "JKSSS", 1100);
 
 CREATE INDEX idx_student_data_app_id ON student_data(application_id);
 
