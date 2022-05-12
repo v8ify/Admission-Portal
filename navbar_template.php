b3fbf8158974316d0e58b9b002f7d3e124a9ed90
Prajwal-Jadhav <jadhavv933@gmail.com>
2022-05-12 23:10:50 +0530

    change styling for navbar

--- navbar_template.php
+++ navbar_template.php
@@ -1,10 +1,12 @@
 <link rel="stylesheet" href="./css/home.css" />
 
-<div class="header">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<div class="header shadow">
     <img src="./css/mescoe-logo.png" alt="logo"/>
     <div class="dropdown">
-        <a class="dropdown-link" href="./logout.php">Home</a>
-        <a class="dropdown-link" href="./check_status.php">Check Status</a>
-        <a class="dropdown-link" href="http://mescoepune.org/contact-new.php" target="_blank">Contact</a>
+        <a class="dropdown-link btn btn-outline-info" href="./logout.php">Home</a>
+        <a class="dropdown-link btn btn-outline-info" href="./check_status.php">Check Status</a>
+        <a class="dropdown-link btn btn-outline-info" href="http://mescoepune.org/contact-new.php" target="_blank">Contact</a>
     </div>
 </div>
